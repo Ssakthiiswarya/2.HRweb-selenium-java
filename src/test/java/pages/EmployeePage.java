323520e8eba6e6514d772ed3400a5954a4ff9391
ishu <ishu@gmail.com>
2026-05-26 11:53:25 +0530

    Completed employee module

--- src/test/java/pages/EmployeePage.java
+++ src/test/java/pages/EmployeePage.java
@@ -1,4 +1,112 @@
 package pages;
 
-public class EmployeePage {
+import base.BasePage;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class EmployeePage extends BasePage {
+
+    WebDriver driver;
+
+    public EmployeePage(WebDriver driver) {
+
+        super(driver);
+
+        this.driver = driver;
+    }
+
+    By pimMenu =
+            By.xpath("//span[text()='PIM']");
+
+    By addEmployeeButton =
+            By.xpath("//a[text()='Add Employee']");
+
+    By firstName =
+            By.name("firstName");
+
+    By lastName =
+            By.name("lastName");
+
+    By saveButton =
+            By.xpath("//button[@type='submit']");
+
+    By employeeName =
+            By.xpath("//h6[text()='Personal Details']");
+
+    public void clickPimMenu() {
+
+        driver.findElement(pimMenu)
+                .click();
+    }
+
+    public void clickAddEmployee() {
+
+        driver.findElement(addEmployeeButton)
+                .click();
+    }
+
+    public void enterFirstName(String fname) {
+
+        driver.findElement(firstName)
+                .sendKeys(fname);
+    }
+
+    public void enterLastName(String lname) {
+
+        driver.findElement(lastName)
+                .sendKeys(lname);
+    }
+
+    public void clickSaveButton() {
+
+        try {
+
+            Thread.sleep(3000);
+
+        } catch (Exception e) {
+
+            e.printStackTrace();
+        }
+
+        JavascriptExecutor js =
+                (JavascriptExecutor) driver;
+
+        js.executeScript(
+                "arguments[0].click();",
+                driver.findElement(saveButton)
+        );
+    }
+
+    public void addEmployee(
+            String fname,
+            String lname) {
+
+        clickPimMenu();
+
+        clickAddEmployee();
+
+        enterFirstName(fname);
+
+        enterLastName(lname);
+
+        clickSaveButton();
+    }
+
+    public String getEmployeePageText() {
+
+        WebDriverWait wait =
+                new WebDriverWait(driver,
+                        Duration.ofSeconds(15));
+
+        wait.until(ExpectedConditions
+                .visibilityOfElementLocated(employeeName));
+
+        return driver.findElement(employeeName)
+                .getText();
+    }
 }
--- src/test/java/tests/EmployeeTest.java
+++ src/test/java/tests/EmployeeTest.java
@@ -1,4 +1,38 @@
 package tests;
 
-public class EmployeeTest {
+import base.BaseTest;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pages.EmployeePage;
+import pages.LoginPage;
+import utilities.ConfigReader;
+
+public class EmployeeTest extends BaseTest {
+
+    @Test
+    public void verifyAddEmployeeTest() {
+
+        LoginPage loginPage =
+                new LoginPage(driver);
+
+        loginPage.loginToApplication(
+                ConfigReader.getUsername(),
+                ConfigReader.getPassword()
+        );
+
+        EmployeePage employeePage =
+                new EmployeePage(driver);
+
+        employeePage.addEmployee(
+                "Ishu",
+                "Sakthi"
+        );
+
+        String currentUrl =
+                driver.getCurrentUrl();
+
+        Assert.assertTrue(
+                currentUrl.contains("pim")
+        );
+    }
 }
